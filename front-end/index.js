4f4725836f0423cbbd6605a562e70263abc5547c
Amalia Claire Fergin <acfergin@msn.com>
2018-09-14 13:49:19 -0700

    sea, mountain and ocean photos added

--- front-end/index.js
+++ front-end/index.js
@@ -41,3 +41,16 @@ $(".myBox").click(function() {
   window.location = $(this).find("a").attr("href");
   return false;
 });
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function() {
+            $("."+selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
