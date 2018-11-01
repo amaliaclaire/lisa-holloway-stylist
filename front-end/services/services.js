d5e7196da47037d84d211902007358cffefb5971
Amalia Claire Fergin <acfergin@msn.com>
2018-11-01 12:26:33 -0700

    copied a accordian from a codepen

--- front-end/services/services.js
+++ front-end/services/services.js
@@ -55,17 +55,11 @@ $(function() {
     });
 });
 
-
-/*!
- * IE10 viewport hack for Surface/desktop Windows 8 bug
- * Copyright 2014-2017 The Bootstrap Authors
- * Copyright 2014-2017 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- */
-
-// See the Getting Started docs for more information:
-// https://getbootstrap.com/getting-started/#support-ie10-width
-
-$('#myCollapsible').collapse({
-  toggle: true
-})
+function toggleIcon(e) {
+      $(e.target)
+          .prev('.panel-heading')
+          .find(".more-less")
+          .toggleClass('glyphicon-plus glyphicon-minus');
+  }
+  $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+  $('.panel-group').on('shown.bs.collapse', toggleIcon);
